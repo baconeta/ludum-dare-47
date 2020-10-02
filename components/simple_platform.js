ea0377ba33f127a4ecc64425c3c8d4a4f82c04ef
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 12:32:40 +1300

    not much yet

--- components/simple_platform.js
+++ components/simple_platform.js
@@ -2,7 +2,7 @@ Crafty.c("simple_platform", {
 	init: function() {
         this.addComponent("2D, DOM, Color, Image, solid, Collision");
         this.x = 15;
-        this.y = 150;
+        this.y = 350;
         this.w = 400;
         this.h = 10;
     }
--- scenes/game.js
+++ scenes/game.js
@@ -1,8 +1,8 @@
 Crafty.defineScene("Game", function() {
 
 	var player = Crafty.e("Hamster");
-	player.color('red')
+	player.color('red');
 	var simp_pf1 = Crafty.e("simple_platform");
-	simp_pf1.color('green')
-
+	simp_pf1.color = 'green'
+	simp_pf1.rotate = 15;
 });
