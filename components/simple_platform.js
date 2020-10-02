48a667d54217b0089ec579b62b53532ae157e664
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 12:39:39 +1300

    refactor component code

--- components/simple_platform.js
+++ components/simple_platform.js
@@ -1,6 +1,8 @@
 Crafty.c("simple_platform", {
 	init: function() {
         this.addComponent("2D, DOM, Color, Image, solid, Collision");
+        this.rotation = 15;
+        this.color('green');
         this.x = -300;
         this.y = 150;
         this.w = 1500;
--- scenes/game.js
+++ scenes/game.js
@@ -3,6 +3,4 @@ Crafty.defineScene("Game", function() {
 	var player = Crafty.e("Hamster");
 	player.color('red');
 	var simp_pf1 = Crafty.e("simple_platform");
-	simp_pf1.color = 'green'
-	simp_pf1.rotate = 15;
 });
