e9c6fa199258e08a2a3e88301efd4f05a360e71a
Steven Rutherford <steve.rutherford12@gmail.com>
2020-10-03 12:30:58 +1300

    movement basics

--- components/hamster.js
+++ components/hamster.js
@@ -1,10 +1,24 @@
 Crafty.c("Hamster", {
 	init: function() {
-        this.addComponent("2D, DOM, Color, Delay, Image, Twoway,  SpriteAnimation, Gravity, Collision, player");
+        this.addComponent("2D, DOM, Color, Delay, Image, Keyboard,  SpriteAnimation, Gravity, Collision, player");
         this.x = 0;
         this.y = 0;
         this.w = 100;
         this.h = 100;
+        this.origin("center")
         this.gravity("solid");
+        this.bind('KeyDown', function(e) {
+		    if(e.key == Crafty.keys.LEFT_ARROW) {
+		      this.ax += -4;
+		      this.rotation += -10;
+		    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+		      this.ax += 4;
+		      this.rotation += 10;
+		    } else if (e.key == Crafty.keys.UP_ARROW) {
+		      this.ay += -4;
+		    } else if (e.key == Crafty.keys.DOWN_ARROW) {
+		      this.ay += 4;
+		    }
+    	});
     }
 })
--- components/simple_platform.js
+++ components/simple_platform.js
@@ -1,9 +1,9 @@
 Crafty.c("simple_platform", {
 	init: function() {
         this.addComponent("2D, DOM, Color, Image, solid, Collision");
-        this.x = 15;
+        this.x = -300;
         this.y = 150;
-        this.w = 400;
+        this.w = 1500;
         this.h = 10;
     }
 })
