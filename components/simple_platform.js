103164f91bece9ab814390cfb77374210bb345b5
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 13:03:05 +1300

    corrected components

--- components/simple_platform.js
+++ components/simple_platform.js
@@ -1,12 +1,12 @@
 Crafty.c("Simple_Platform", {
 	init: function() {
-        this.addComponent("2D, DOM, Color, WiredHitBox");
+        this.addComponent("2D, DOM, Color, solid, WiredHitBox");
         this.x = -300;
         this.y = 250;
         this.w = 1500;
         this.h = 10;
         this.rotation = 15;
         this.color('green');
-        this.debugStroke('blue')
+        this.debugStroke('black');
     }
 })
--- scenes/game.js
+++ scenes/game.js
@@ -3,5 +3,4 @@ Crafty.defineScene("Game", function() {
 	var player = Crafty.e("Hamster");
 	player.color('red');
 	var simp_pf1 = Crafty.e("Simple_Platform");
-	simp_pf1.addComponent('solid')
 });
