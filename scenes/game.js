5070c8a17e38400a153c65c883d89ce602ee4662
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 15:18:21 +1300

    renamed coding to fit simpler conventions - pet into hamster, hamster into player

--- scenes/game.js
+++ scenes/game.js
@@ -1,14 +1,13 @@
 Crafty.defineScene("Game", function() {
 
-	var player = Crafty.e("Hamster");
+	var player = Crafty.e("Player");
 	player.color('red')
 	var wheel = Crafty.e("Wheel");
 	player.attach(wheel);
-	var pet = Crafty.e("Pet");
-	player.attach(pet)
-	pet.x = 25;
-	pet.y = 60;
+	var hamster = Crafty.e("Hamster");
+	player.attach(hamster)
+	hamster.x = 25;
+	hamster.y = 60;
 
 	var simp_pf1 = Crafty.e("Simple_Platform");
-	simp_pf1.color('green')
 });
