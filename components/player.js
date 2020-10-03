c7d456cc975b85692042f450472fd4f47cc89ee8
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 22:16:31 +1300

    fixed error with player going through floor

--- components/player.js
+++ components/player.js
@@ -1,5 +1,8 @@
 Crafty.c("Player", {
 	init: function() {
+		this.direction_force = 0
+		this.force_level_x = 0
+		this.holding_key = false;
         this.addComponent("2D, DOM, Color, Keyboard, Matter");
         this.attr({
             x : 0,
@@ -14,8 +17,6 @@ Crafty.c("Player", {
                 label : 'player'
             }
         });
-		this.holding_key = false;
-		this.force_level_x = 0;
         this.bind('KeyDown', function(e) {
 		    if(e.key == Crafty.keys.LEFT_ARROW) {
 				this.holding_key = true;
@@ -30,12 +31,12 @@ Crafty.c("Player", {
 		this.bind('KeyUp', function(e) {
 			if(e.key == Crafty.keys.LEFT_ARROW || e.key == Crafty.keys.RIGHT_ARROW) {
 				this.holding_key = false;
-		}
+			}
 		});
         this.bind('EnterFrame', function(e) {
             Crafty.trigger("PlayerVelocity", this._body.velocity);
 			if (this.holding_key == true) {
-				Matter.Body.applyForce(this._body, {x: this.x+this.direction_force, y: this.y+5}, {x: this.force_level_x, y: 0});
+				Matter.Body.applyForce(this._body, {x: this.x+this.direction_force, y: this.y}, {x: this.force_level_x, y: 0});
 			}
         });
     }
