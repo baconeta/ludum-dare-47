7a55612d06e23957a590f2f433ce8503579d51e3
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-03 21:59:48 +1300

    added tunnel images
    and changed key press to key hold functionality

--- components/player.js
+++ components/player.js
@@ -14,14 +14,29 @@ Crafty.c("Player", {
                 label : 'player'
             }
         });
+		this.holding_key = false;
+		this.force_level_x = 0;
         this.bind('KeyDown', function(e) {
 		    if(e.key == Crafty.keys.LEFT_ARROW) {
-				Matter.Body.applyForce(this._body, {x: this.x+10, y: this.y}, {x: -30, y: 0});
+				this.holding_key = true;
+				this.direction_force = 10
+				this.force_level_x = -5
 		    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-				Matter.Body.applyForce(this._body, {x: this.x-10, y: this.y}, {x: 30, y: 0});		    }
-    	})
+				this.directional_force = -10
+				this.force_level_x = 5
+				this.holding_key = true;
+			}
+    	});
+		this.bind('KeyUp', function(e) {
+			if(e.key == Crafty.keys.LEFT_ARROW || e.key == Crafty.keys.RIGHT_ARROW) {
+				this.holding_key = false;
+		}
+		});
         this.bind('EnterFrame', function(e) {
             Crafty.trigger("PlayerVelocity", this._body.velocity);
+			if (this.holding_key == true) {
+				Matter.Body.applyForce(this._body, {x: this.x+this.direction_force, y: this.y+5}, {x: this.force_level_x, y: 0});
+			}
         });
     }
 })
