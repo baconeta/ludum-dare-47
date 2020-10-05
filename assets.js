2486e6c2c1abd796a7d1f31c3e45bc598e51233b
Steven Rutherford <steve.rutherford12@gmail.com>
2020-10-05 20:38:55 +1300

    add ladder

--- assets.js
+++ assets.js
@@ -130,5 +130,10 @@ var gameAssets = {
            "tileh": 127,
            "map": {"play_again_button": [0, 0]}
        },
+       "assets/images/grate_platform.png": {
+           "tile" : 876,
+           "tileh": 115,
+           "map": {"ladder": [0, 0]}
+       },
    }
 }
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var GAME_SCREEN_WIDTH = 900;
 var GAME_SCREEN_HEIGHT = 600;
 var CURRENT_LEVEL = 0;
 Crafty.Matter.init({
-	//debug : true,
+	// debug : true,
 	gravity : {
 	  x : 0,
 	  y : 0.098
