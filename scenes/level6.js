fdc6a3a65fc401e13f04976c0035d50a809545b4
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-05 23:00:13 +1300

    fixed 8 levels

--- scenes/level6.js
+++ scenes/level6.js
@@ -10,7 +10,7 @@ Crafty.defineScene("Level6", function() {
         if (next_level_loading == false) {
             next_level_loading = true;
             setTimeout(function () {
-                Crafty.scene("EndScreen");
+                Crafty.scene("Level7");
             }, 500);
         }
     });
