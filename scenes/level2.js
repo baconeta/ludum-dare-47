876de00cd57e19e73d72df41be119abbf695ca3c
Josh Pearson <joshuapearsonnz@gmail.com>
2020-10-05 14:22:09 +1300

    fixed a small level movement bug

--- scenes/level2.js
+++ scenes/level2.js
@@ -4,27 +4,18 @@ Crafty.defineScene("Level2", function() {
     var level_details = {x:0,y:-200,w:4000,h:1000}
     var background = Crafty.e("Background").place(level_details.x,level_details.y,level_details.w,level_details.h);
     background.set_boundaries();
-    var next_level_door = Crafty.e("Door").place(1000,level_details.y+level_details.h-100, true); //Give the door a location (the y parameters are set cleanly for the floor here)
+    var next_level_door = Crafty.e("Door").place(1300,level_details.y+level_details.h-100, true); //Give the door a location (the y parameters are set cleanly for the floor here)
+    Crafty.bind("NextLevel", function() {
+        setTimeout(function () {
+            Crafty.scene("Level2");
+        }, 500);
+    });
     //var level_controller = Crafty.e("Level_Controller");
 
     // Object and obstacle placements //
     var simp_pf1 = Crafty.e("Platform").place(400,300);
-    var simp_pf1 = Crafty.e("Platform").place(650,300);
-    var tunnel1 = Crafty.e("Tunnel").place(850,300);
-    var simp_pf1 = Crafty.e("Platform").place(1020,300);
-    var simp_pf1 = Crafty.e("Platform_Ramp_Right").place(1265,210);
-    var simp_pf1 = Crafty.e("Platform").place(2000,170);
-    var simp_pf1 = Crafty.e("Platform").place(2250,170);
-    var simp_pf1 = Crafty.e("Tunnel").place(2500,170);
-    var simp_pf1 = Crafty.e("Tunnel").place(2660,170);
-    var simp_pf1 = Crafty.e("Platform_Ramp_Right").place(2910,170);
-    var simp_pf1 = Crafty.e("Platform").place(2000,650);
-    var simp_pf1 = Crafty.e("Platform").place(2250,550);
-    var simp_pf1 = Crafty.e("Platform").place(2500,500);
-    var simp_pf1 = Crafty.e("Platform").place(2750,500);
-    var simp_pf1 = Crafty.e("Platform").place(3000,500);
-    var house = Crafty.e("House").place(1500, 500);
-    var rock = Crafty.e("Rock").place(300,650);
+
+    var rock = Crafty.e("Rock").place(700,650);
 
     // Hamster and Wheel Code //
     var hamster = Crafty.e("HamsterWrapper");
